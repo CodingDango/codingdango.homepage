2cf4960a1e1e93ec5a46eaf4ad8ab27e030c13ee
CodingDango <babyyodaman2.0@gmail.com>
2024-12-12 22:17:18 +0800

    Add files via upload

--- convert.py
+++ convert.py
@@ -1,24 +1,15 @@
-from PIL import Image
-
-# img = Image.open('')
-
-# img = img.convert('RGB')
-
-# pixels = img.load()
-
-# Open the image with transparency
-
-
-png = Image.open('python.png').convert('RGBA')
-background = Image.new('RGBA', png.size, (255,255,255))
-
-alpha_composite = Image.alpha_composite(background, png)
-alpha_composite.save('foo.png', 'PNG', quality=80)
+# TODO: Create the logging_decorator() function 👇
+def logging_decorator(function):
+    def wrapper(*args):
+        print(f"You called {function.__name__}{str(args)}")
+        print(f"It returned: {function(*args)}")
+    return wrapper    
+    
+# TODO: Use the decorator 👇
+@logging_decorator
+def a_function(*args):
+    return sum(args)
+    
+a_function(4,5,6,7)
 
 
-# for i in range(img.size[0]):
-#     for j in range(img.size[1]):
-#         if pixels[i, j] == (0,0,0) or pixels[i, j] == (89,89,89):
-#             pixels[i, j] = (255,255,255)
-            
-# img.save('modified_image.png')
